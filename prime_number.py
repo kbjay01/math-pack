456fe5a5600a5cd3c2520a9fdaeb7a2815a260e7
Jakub <39642297+kbjay01@users.noreply.github.com>
2019-01-04 12:02:51 +0100

    Update prime_number.py

--- prime_number.py
+++ prime_number.py
@@ -1,6 +1,7 @@
 p_num = 0
 isPrime = 0
-def prime_numb(): 
+def prime_numb():
+    counter = 0 
     for i in range(1,p_num):
         if (i>p_num):
             break;
@@ -11,6 +12,4 @@ def prime_numb():
         isPrime = 1
     else:
         isPrime = 0
-
-
-
+  
