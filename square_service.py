4f62445572773f15ae6c00bf6d9144f7abb40e43
Jakub <39642297+kbjay01@users.noreply.github.com>
2019-03-02 13:11:05 +0100

    Update square_service.py

--- square_service.py
+++ square_service.py
@@ -1,23 +1,22 @@
 import math
-
-num = 0
-a = 2.0
-b = 1.0
-def sqr_svc():
+def sqr_svc(num):
 
   perfect_num = math.sqrt(num)
+  a = 2.0
+  b = 1.0
   while (True):
 
     if (a * math.sqrt(b) < perfect_num):
-        b = b + 1
-        #print("if pierwszy")
+      b += 1
 
     if (a * math.sqrt(b) > perfect_num):
-        a = a+1
-        b = 1
-        #print(str(a) + "," + str(b))
+      a += 1
+      b = 1
+
+    if (a * math.sqrt(b) > (perfect_num)) and b == 1:
+      return num
+      break
 
     if (a * math.sqrt(b) == perfect_num):
-        break
-
-  print(str(a) + "√" + str(b))
+      output = str(int(a)) + chr(8730) + str(int((b)))
+      return output
