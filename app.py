7d367cb9d031a147453aa69f3cf0fda2cf91342d
Jakub <39642297+kbjay01@users.noreply.github.com>
2019-03-08 00:23:43 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 import quadratic_function,segment_length
 
-print("What do you want to do?")
-print("1. Let's calculate quadratic_function")
-print("2. Let's calculate length of a segment")
-choice = input(">> ")
+def main_menu():
+    print("What do you want to do?")
+    print("1. Let's calculate quadratic function")
+    print("2. Let's calculate length of a segment")
+    choice = input(">> ")
 
-if choice == "1":
-    quadratic_function.quad_func()
-elif choice == "2":
-    segment_length.len_seg()
-else:
-    print("[!] Unknown choice")
+    if choice == "1":
+        quadratic_function.quad_func()
+    elif choice == "2":
+        segment_length.len_seg()
+    else:
+        print("[!] Unknown choice \n----------\n")
+        main_menu()
+
+main_menu()
